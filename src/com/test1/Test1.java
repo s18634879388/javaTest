f98fd63fc5717cf6c2b8bef6752c748a3c329a50
史小绮 <shixiaoqi@9hcom.com>
2017-06-12 10:57:48 +0800

    git test

---
+++ src/com/test1/Test1.java
@@ -0,0 +1,10 @@
+package com.test1;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+    }
+}
