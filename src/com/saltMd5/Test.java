0c8819effb6dec588d8cd9cf05ecbd508961db97
shixiaoqi <xiaoqi235689@qq.com>
2017-07-20 16:36:19 +0800

    pppp

--- src/com/saltMd5/Test.java
+++ src/com/saltMd5/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args) {
         /*String s1 = "111112";
         String s2 = Md5Utils.md5(s1);
-        System.out.println("加密后密码s2:"+s2);
+        System.out.println("加密后密码s2:"+s2);//ppppppppp
 
         String s3 = Md5Utils.md5(s2);
         System.out.println("解密后密码s3:"+s3);
